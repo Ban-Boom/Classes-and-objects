420949e6c9334fa967873dcb82a08c91be4ad8e2
Bronislav <bronkyr59@gmail.com>
2025-03-14 22:05:47 +0300

    Error correction

--- Two-number Calculator/Калькулятор двух чисел/Калькулятор двух чисел.cpp
+++ Two-number Calculator/Калькулятор двух чисел/Калькулятор двух чисел.cpp
@@ -6,9 +6,22 @@ private:
     double num1, num2;
 
 public:
-    Calculator(double n1, double n2) {
-        num1 = (n1 != 0) ? n1 : 1;
-        num2 = (n2 != 0) ? n2 : 1;
+    Calculator() : num1(0), num2(1) {}
+
+    bool set_num1(double n) {
+        if (n != 0) {
+            num1 = n;
+            return true;
+        }
+        return false;
+    }
+
+    bool set_num2(double n) {
+        if (n != 0) {
+            num2 = n;
+            return true;
+        }
+        return false;
     }
 
     double add() { return num1 + num2; }
@@ -17,38 +30,23 @@ public:
     double subtract_2_1() { return num2 - num1; }
     double divide_1_2() { return num1 / num2; }
     double divide_2_1() { return num2 / num1; }
-
-    bool set_num1(double n1) {
-        if (n1 != 0) {
-            num1 = n1;
-            return true;
-        }
-        return false;
-    }
-
-    bool set_num2(double n2) {
-        if (n2 != 0) {
-            num2 = n2;
-            return true;
-        }
-        return false;
-    }
 };
 
 int main() {
+    Calculator calc;
     double n1, n2;
+
     while (true) {
         cout << "Enter num1: ";
         cin >> n1;
         cout << "Enter num2: ";
         cin >> n2;
 
-        if (n1 == 0 || n2 == 0) {
+        if (!calc.set_num1(n1) || !calc.set_num2(n2)) {
             cout << "Incorrect input!" << endl;
             continue;
         }
 
-        Calculator calc(n1, n2);
         cout << "num1 + num2 = " << calc.add() << endl;
         cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
         cout << "num2 - num1 = " << calc.subtract_2_1() << endl;
@@ -58,4 +56,3 @@ int main() {
     }
     return 0;
 }
-
